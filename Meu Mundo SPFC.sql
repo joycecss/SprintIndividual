765ee2ae8c0b2cab92b42e3ef2a7a7a94d8d4743
joyce cristyna <joohcristyna@gmail.com>
2021-05-27 18:36:41 -0300

    Atualizado 27/05

--- Meu Mundo SPFC.sql
+++ Meu Mundo SPFC.sql
@@ -6,18 +6,7 @@ create table usuario(
     nome varchar(45),
     login varchar(45),
     senha varchar(45)
-   -- dtNascimento date,
-    -- sexo varchar(15),
-   -- check (sexo = 'feminino' or sexo = 'masculino' or sexo = 'outros'),
-   -- saoPaulino char(3),
-  --  check (saoPaulino = 'Sim' or saoPaulino = 'Não') 
 );
-
-create table login (
-	idLogin int primary key not null auto_increment,
-    email varchar(45),
-    senha varchar(45)
-)auto_increment = 100;
 
 drop database meumundospfc;
 select * from usuario;
